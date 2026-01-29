abb35869636a523df6ca01273c76618f542588de
akinakamu <akiyo.nakamura@stmn.co.jp>
2026-01-29 18:11:16 +0900

    fix: android add CollapseKey

--- server/android_notification_server.go
+++ server/android_notification_server.go
@@ -173,7 +173,7 @@ func (me *AndroidNotificationServer) SendNotification(msg *PushNotification) Pus
 	if msg.SubType == "calls" || msg.SubType == "calls_ended" {
 		d := TTLForCallsSeconds * time.Second
 		fcmMsg.Android.TTL = &d
-		// fcmMsg.Android.CollapseKey = msg.DeviceID
+		fcmMsg.Android.CollapseKey = msg.DeviceID
 	}
 
 	me.logger.Info(
